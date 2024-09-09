6e56e96b3f98f5ac49c107e8de67981e837ff449
shahbazansarireplit <shahbazansarireplit@gmail.com>
2024-09-09 17:07:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
  <head> 
   <meta charset="utf-8"> 
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-  <title>TECHNICAL ABHI2M SERVER 😎💔</title> 
+  <title>TECHNICAL ABHI2M SERVER</title> 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> 
   <style>
     body {
